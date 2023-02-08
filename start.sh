5ab48ee6e3b3a7734d52c141c5ffe9fc51f6f4e3
Abdulrahman Kazamel <50080379+Abdulrahman-Kazamel@users.noreply.github.com>
2023-02-08 09:48:58 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#this Script to be run in demo Enviremont for starting and stoping EC and the controller
+
+cd /opt/appdynamics/platform/platform-admin
+
+bin/platform-admin.sh login -u admin -p AppD123
+
+bin/platform-admin.sh stop-platform-admin
+bin/platform-admin.sh start-platform-admin
+
+bin/platform-admin.sh stop-controller-db
+bin/platform-admin.sh start-controller-db
+
+bin/platform-admin.sh stop-controller-appserver
+bin/platform-admin.sh start-controller-appserver
+ 
+netstat -ntlp |grep -i 9191
+ curl 127.0.0.1:9191
+ 
